a55f3c23d8a84adc0d4f34fb3cd449bf32fa4072
tomatomaster <iiitomatomasteriii@gmail.com>
2017-05-22 20:53:53 +0900

    modify function name

--- gotorial/errutil.go
+++ gotorial/errutil.go
@@ -3,15 +3,15 @@ package gotorial
 import "os"
 import "fmt"
 
-//OSExit exit app if err != nil
-func OSExit(err error) {
+//OSExitIfError exit app if err != nil
+func OSExitIfError(err error) {
 	if err != nil {
 		os.Exit(1)
 	}
 }
 
-//OSExitWithCause print cause and exit app if err != nil
-func OSExitWithCause(cause string, err error) {
+//OSExitWithCauseIfError print cause and exit app if err != nil
+func OSExitWithCauseIfError(cause string, err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v %v", cause, err)
 		os.Exit(1)
